71457aeac1561a39e9d1fd06591a221e9fbc445c
boutiqueprojet6-dot <boutiqueprojet6@gmail.com>
2026-09-03 06:02:11 +0000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BoutiqueApp from "./BoutiqueApp.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BoutiqueApp />
+  </React.StrictMode>
+);
