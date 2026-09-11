373efc18178356c8e607db9e79ad21b3c6062e9a
boutiqueprojet6-dot <boutiqueprojet6@gmail.com>
2026-09-11 21:41:30 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "icon-192.png", "icon-512.png"],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+      },
       manifest: {
         name: "Shopnify",
         short_name: "Shopnify",
