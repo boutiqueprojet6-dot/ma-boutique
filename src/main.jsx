663d14adb8026af7b95b9002369e369c25c1ec98
boutiqueprojet6-dot <boutiqueprojet6@gmail.com>
2026-09-12 05:48:53 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import BoutiqueApp from "./BoutiqueApp.jsx";
 import "./index.css";
 
+const isCapacitorApp = typeof window !== "undefined" && !!window.Capacitor;
+
+if (!isCapacitorApp && "serviceWorker" in navigator) {
+  import("virtual:pwa-register").then(({ registerSW }) => {
+    registerSW({ immediate: true });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BoutiqueApp />
